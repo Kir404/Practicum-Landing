7e7742c6275d69bdfe6bcd204dd94d97adc052a0
Kirina Viktoria <kirinavi@outlook.com>
2022-12-10 11:28:51 +0300

    add faq block

--- script.js
+++ script.js
@@ -6,9 +6,24 @@ const hero = document.querySelector('#hero');
 const imgBurger = document.querySelector('#burger-btn');
 const imgBurgerClose = document.querySelector('#burger-close');
 
+
 burgerBtn.addEventListener('click', () => {
    dropDown.classList.toggle('hide');
    hero.classList.toggle('blur');
    imgBurger.classList.toggle('hide');
    imgBurgerClose.classList.toggle('hide');
+})
+
+
+// Завернуть в функцию для работы с несколькими кнопками
+const faqBtn = document.querySelector('#faq-btn');
+const faqInner = document.querySelector('#faq-inner');
+const imgFaqOpen = document.querySelector('#faq-open'); 
+const imgFaqClose = document.querySelector('#faq-close'); 
+
+faqBtn.addEventListener('click', () =>{
+   faqInner.classList.toggle('hide');
+   imgFaqClose.classList.toggle('hide');
+   imgFaqOpen.classList.toggle('hide');
+
 })
