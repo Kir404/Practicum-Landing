172b0faa02fb31f6d912282facfbe51787906049
Kirina Viktoria <kirinavi@outlook.com>
2022-12-12 21:37:25 +0300

    edit faq block

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const hero = document.querySelector('#hero');
 const imgBurger = document.querySelector('#burger-btn');
 const imgBurgerClose = document.querySelector('#burger-close');
 
-
 burgerBtn.addEventListener('click', () => {
    dropDown.classList.toggle('hide');
    hero.classList.toggle('blur');
@@ -14,16 +13,16 @@ burgerBtn.addEventListener('click', () => {
    imgBurgerClose.classList.toggle('hide');
 })
 
-
-// Завернуть в функцию для работы с несколькими кнопками
-const faqBtn = document.querySelector('#faq-btn');
-const faqInner = document.querySelector('#faq-inner');
-const imgFaqOpen = document.querySelector('#faq-open'); 
-const imgFaqClose = document.querySelector('#faq-close'); 
+//функция для работы с несколькими блоками FAQ
+document.querySelectorAll('#faq-block').forEach(function(faqWrapper){
+const faqBtn = faqWrapper.querySelector('#faq-btn');
+const faqInner = faqWrapper.querySelector('#faq-inner');
+const imgFaqOpen = faqWrapper.querySelector('#faq-open'); 
+const imgFaqClose = faqWrapper.querySelector('#faq-close'); 
 
 faqBtn.addEventListener('click', () =>{
    faqInner.classList.toggle('hide');
    imgFaqClose.classList.toggle('hide');
    imgFaqOpen.classList.toggle('hide');
-
+})
 })
